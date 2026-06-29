fa0289fafd4c378121a7387f1d7ca9f6410ac65e
Fardin Hossain <iamfardin.swe@gmail.com>
2026-06-30 01:30:38 +0600

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import SectionOrganizerPage from "./components/SectionOrganizerPage";
 import "./index.css";
@@ -19,5 +20,6 @@ function Root() {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Root />
+    <Analytics />
   </React.StrictMode>,
 );
